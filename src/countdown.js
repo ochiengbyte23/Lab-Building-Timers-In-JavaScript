c3694bdf36bddaea2d236e2064a6326e3dee81e2
Bildad Ochieng Masaga <bijasoto@gmail.com>
2025-12-16 15:34:35 +0300

    completed the countdownTimer function which decrements the starting time at an interval

--- src/countdown.js
+++ src/countdown.js
@@ -13,10 +13,20 @@
  */
 function countdownTimer(startTime, interval) {
   // Initialize the remaining time
+  let remainingTime = startTime;
   // Set up a timer using setInterval
   // Log the remaining time and decrement it
-  // Stop the timer when time reaches 0
+  const timerId = setInterval(() => {
+    console.log(remainingTime);
+    remainingTime--;
+    // Stop the timer when time reaches 0
+    if (remainingTime <= 0) {
+      clearInterval(timerId)
+    }
+  }, interval);
+
   // Return the timer ID for validation
+  return timerId;
 }
 
 module.exports = { countdownTimer };
